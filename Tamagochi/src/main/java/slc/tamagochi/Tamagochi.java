8da7a6fd77eff5bd7e999a86284ad86d5c1db235
Serlopcas <serlopcas.5@gmail.com>
2025-01-29 09:48:43 +0100

    Just create the proyect with Netbeans.

---
+++ Tamagochi/src/main/java/slc/tamagochi/Tamagochi.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package slc.tamagochi;
+
+/**
+ *
+ * @author serlo
+ */
+public class Tamagochi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
